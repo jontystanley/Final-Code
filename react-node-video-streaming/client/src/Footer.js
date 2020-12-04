067039d6614a3f4768bd34c4e45339af69fe419a
Jayant Jonathan Stanley <neonhitch64@gmail.com>
2020-12-04 22:28:02 +0530

    First commit

---
+++ react-node-video-streaming/client/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Footer() {
+    return (
+        <footer>
+            &copy; Made with ❤️ from Jonty Stanley :)  IconsTeams 2020 &copy;
+        </footer>
+    );
+}
+export default Footer;
---
+++ react-node-video-streaming/client/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Header() {
+    return (
+        <header>
+            Live Streaming Service
+        </header>
+    );
+}
+export default Header;
